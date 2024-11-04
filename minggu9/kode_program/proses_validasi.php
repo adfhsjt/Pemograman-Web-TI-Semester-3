4b404cacb7b2b5e142dfb89a884c24773014cfc1
ahmaddzulfadhlihannan <ahmadriski207@gmail.com>
2024-11-04 15:31:30 +0700

    P10 1

--- minggu9/kode_program/proses_validasi.php
+++ minggu9/kode_program/proses_validasi.php
@@ -7,10 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($username) || empty($password)) {
         echo 'Harus terisi';
-    } elseif (strlen($password) > 6) {
-        echo 'Password maksimal 6 karakter';
-    } elseif (!preg_match("/[a-z]/", $password) || !preg_match("/[A-Z]/", $password)) {
-        echo 'Password harus terdiri dari huruf besar dan kecil';
+    } elseif (strlen($password) < 5) {
+        echo 'Password minimal 5 karakter';
+    } elseif (!preg_match("/[a-z]/", $password) || !preg_match("/[0-9]/", $password)) {
+        echo 'Password harus terdiri dari huruf besar, kecil, dan';
     } else {
         $_SESSION['username'] = $username;
         echo 'Login berhasil, redirecting...';
--- minggu9/kode_program/validasi.php
+++ minggu9/kode_program/validasi.php
@@ -8,10 +8,10 @@ if (isset($_POST['login'])) {
 
     if (empty($username) || empty($password)) {
         $error = "Harus terisi";
-    } elseif (strlen($password) > 6) {
-        $error = "Password maksimal 6 karakter";
-    } elseif (!preg_match('/[a-z]/', $password) || !preg_match('/[A-Z]/', $password)) {
-        $error = "Password harus terdiri dari huruf besar dan kecil";
+    } elseif (strlen($password) < 5) {
+        $error = "Password minimal 5 karakter";
+    // } elseif (!preg_match('/[a-z]/', $password) || !preg_match('/[A-Z]/', $password)) {
+    //     $error = "Password harus terdiri dari huruf besar dan kecil";
     } else {
         $_SESSION['username'] = $username;
         header('Location: home.php');
